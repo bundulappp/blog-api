872b42904c10384c09d4516617dbff81a662ea24
Patrik Bundula <bundulappp@gmail.com>
2023-06-21 14:23:55 +0200

    add jwt logic

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { userProviders } from './entities/user.providers';
+import { JwtAuthGuard } from './services/authGuard';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [UsersController],
-  providers: [...userProviders, UsersService],
+  providers: [...userProviders, UsersService, JwtAuthGuard],
 })
 export class UsersModule {}
