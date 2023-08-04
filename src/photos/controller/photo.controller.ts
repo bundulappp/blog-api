4ba78bf02da6737f611d1a6d77c02e32414c4ae9
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-08-04 14:58:24 +0200

    photo upload method extended

--- src/photos/controller/photo.controller.ts
+++ src/photos/controller/photo.controller.ts
@@ -6,18 +6,36 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
+import { of } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/profileImages',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+    },
+  }),
+};
+//todo authGuard
+//https://www.youtube.com/watch?v=f-URVd2OKYc&t=388s
 @Controller('photo')
 export class PhotoController {
   @Post('upload')
-  @UseInterceptors(FileInterceptor('photo', { dest: './uploads' }))
+  @UseInterceptors(FileInterceptor('photo', storage))
   uploadSingle(@UploadedFile() file) {
-    console.log(file);
+    return of({ imagePath: file.filename });
   }
 
-  @Post('uploads')
-  @UseInterceptors(FilesInterceptor('photos[]', 10, { dest: './uploads' }))
-  uploadMultiple(@UploadedFiles() files) {
-    console.log(files);
-  }
+  // @Post('uploads')
+  // @UseInterceptors(FilesInterceptor('photos[]', 10, { dest: './uploads' }))
+  // uploadMultiple(@UploadedFiles() files) {
+  //   console.log(files);
+  // }
 }
