04c82addea653cce785ac488503f2810d398b62c
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-08-08 13:13:24 +0200

    add uploadSingle method

--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PhotoEntity } from 'src/entities/photo.entity';
+import { PhotoUploadDto } from '../models/photo-upload.dto';
+import { UsersEntity } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PhotosService {
-  constructor(@InjectRepository(PhotoEntity) private photoRepository) {}
+  constructor(
+    @InjectRepository(PhotoEntity)
+    private photoRepository: Repository<PhotoEntity>,
+  ) {}
+
+  async uploadSingle(photoDto: PhotoUploadDto, userEntity: UsersEntity) {
+    const photoEntity = new PhotoEntity();
+    photoEntity.filename = photoDto.filename;
+    photoEntity.originalname = photoDto.originalname;
+    photoEntity.mimetype = photoDto.mimetype;
+    photoEntity.path = photoDto.path;
+    photoEntity.size = photoDto.size;
+    photoEntity.user = userEntity;
+
+    return await this.photoRepository.save(photoEntity);
+  }
 }
